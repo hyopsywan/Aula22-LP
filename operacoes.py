7e8eaa27048b0815afad0cd73dfbd888f7663f71
hyopsywan <heloisataraujo14@gmail.com>
2025-03-13 13:21:08 -0300

    operacoes.py

---
+++ operacoes.py
@@ -0,0 +1,14 @@
+# Aula 22- LP
+def soma(a,b):
+    return a + b
+
+def subtracao(a,b):
+    return a - b
+
+def multiplicacao(a,b):
+    return a * b
+
+def divisao(a,b):
+    if b == 0
+        return "Erro: Nenhum numero pode ser dividio por zero."
+    return a / b   
