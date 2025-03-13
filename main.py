f3c111e3fa11004a55afd01a4c9f6cfba8e18c5b
hyopsywan <heloisataraujo14@gmail.com>
2025-03-13 13:20:34 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from operacoes import soma,subtracao,multiplicacao,divisao
+from utils import exibir_resultado
+operacao= input("escolha uma operacao (+,-,*,/)")
+a = int (input ("Digite um valor:"))
+b = int (input ("Digite um segundo valor:"))
+
+if operacao == "+"
+    resultado = soma (a,b)
+
+elif operacao == "+":
+    resultado = soma (a,b)
+
+elif operacao == "-":
+    resultado = subtracao (a,b)
+
+elif operacao == "*":
+    resultado = multiplicacao (a,b)
+
+elif operacao == "/": 
+    resltado = divisao (a,b)   
+
+    exibir_resultado (operacao, resultado)
